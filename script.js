1b1afcad57eb739649c8f3d68542195759592f15
JansenCk <caik11@hotmail.com>
2022-02-05 13:18:03 -0300

    Reorganizar JS - passo a passo

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 let minhasCartas=[];
-let cartasEmJogo = ['devNinja','devNinja','devNinja','devNinja','devNinja','devNinja','devNinja',];
+let cartasEmJogo = ['carta1','carta2','carta3','carta4','carta5','carta6','carta7',];
 
-/*Escolher o numero de cartas do jogo*/
+
+// Layout da página: Desktop e Mobile Ok
+// Prompt: Quantas cartas terá o jogo? Ok
+
+// Armazenar quantidade de cartas escolhida
+// Adicionar quantidade de cartas na mesa
+// Rotacionar cartas e aparecer imagem
+// Cartas iguais permanecem viradas e diferentes desviram
+// Armazenar cartas igual e subtrai-las do jogo
+// Todas as cartas deram match = fim de jogo 
+
+
 function iniciarJogo(){
     let numeroCartas = parseInt(prompt("Escolhar e digite um número par de cartas entre 4 e 14"));
 
     while((numeroCartas % 2 !== 0) || (numeroCartas < 4) || (numeroCartas > 14)){
         numeroCartas = parseInt(prompt("Escolha um número par de cartas entre 4 e 14."));
-    }
-    for(i=0; (i<numeroCartas/2); i++) {
-
-        minhasCartas.push(cartasEmJogo[i]);
-        minhasCartas += minhasCartas.push(cartasEmJogo[i])
     }
 }
 iniciarJogo(); 
